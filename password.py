c32f47a8df35b334b4ad2f42439c5deb1d869ebd
Jackson Sommerich <Jackson-S@users.noreply.github.com>
2016-07-22 13:00:11 +1000

    Added support for windows

--- password.py
+++ password.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 from random import randint, choice
-from sys import argv
-from os import system
+from sys import argv, platform
+from os import path
 
 """ python3 password.py [dictionary]
 
@@ -9,8 +10,26 @@ from os import system
     pointing to a correct dictionary)
 """
 
-dictionary = open(argv[1], "r") if len(argv) > 1 else open("/usr/share/dict/web2", "r").read().split("\n")
-password, length, maxchars = "", 0, 0
+def paste(text):
+    from os import system
+    pastecmd = {"darwin": "pbcopy", "win32": "clip"}
+    if platform in pastecmd:
+        system('echo "{}" | {}'.format(text, pastecmd[platform]))
+    else:
+        raise "PlatformError"
+        
+
+if platform == "win32" and len(argv) == 1:
+    input("Windows requires a dictionary file to be located in the first argument!\nPress any key to continue...")
+    quit()
+try:
+    dictionary = open(argv[1], "r") if len(argv) > 1 else open(path.join("/", "usr", "share", "dict", "words"), "r")
+    dictionary = dictionary.read().split("\n")
+except FileNotFoundError:
+    input("Dictionary file not found, try specifying one in arguments or check arguments are correct...")
+    quit()
+
+length, maxchars = 0, 0
 additions = [chr(x) for x in range(33, 65)]
 errors = ("No", "Bad", "Incorrect", "Rude", "Wrong", "Silly", "Spurious", ":/")
 
@@ -18,11 +37,12 @@ while (True):
     try:
         length = int(input("How many words? "))
         extras = int(input("How many extra characters would you like to add? "))
-        maxchars = int(input("Maximum password length? (minumum {}) ".format(length * 4 + extras)))
-        if length < 1 or maxchars < 1 or (maxchars < length * 4):
-            print("Length must be 4 * words + 1 i.e. 4 words, 17 characters")
+        maxchars = int(input("Maximum password length? (minumum {}) ".format((length * 4) + extras)))
+        if length < 1 or 1 < maxchars < length * 4 + extras:
+            print("Length must be 4 * words + extra characters i.e. 4 words, 17 characters")
             continue
-        break
+        else:
+            break
     except (ValueError, NameError):
         print("{} horse battery staple, that's not a number!".format(choice(errors)))
         continue
@@ -45,6 +65,10 @@ while(True):
                 print("{esc}[97m{l}{esc}[{prev}m".format(esc=chr(27), l=letter, prev=colour[cindex]), end="")
             else:
                 print("{}{}".format(chr(27) + "[" + str(colour[cindex]) + "m", letter), end="")
-        system("echo {} | pbcopy".format("".join(password)))
-        print(chr(27) + "[39m\nPassword copied to clipboard.")
+        print(chr(27) + "[39m", end="")
+        try:
+            paste("".join(password))
+            print("Password copied to clipboard...")
+        except:
+            pass
         break
